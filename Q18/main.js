d7e0ada2008bb82e1714e0e904273268f1cb3b89
Shehadeh-Al-Momani <shehadeh.almomani@gmail.com>
2020-12-10 23:26:15 +0200

    Solve Q17

--- Q18/main.js
+++ Q18/main.js
@@ -7,11 +7,14 @@ Given an integer, n, return n! (n factorial)
 5! = 5 * 4 * 3 * 2 * 1
 */
 
-function factorial() {
-  // YOUR CODE HERE
+function factorial(n) {
+  if (n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1)
 }
 
-/* 
+/*
 Examples:
 factorial(1) // => 1
 factorial(3) // => 6
