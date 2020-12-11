55c20c71690dadd40d54dceb3abad528e511b38a
Shehadeh-Al-Momani <shehadeh.almomani@gmail.com>
2020-12-11 18:52:28 +0200

    Solve Q23

--- Q23/main.js
+++ Q23/main.js
@@ -3,16 +3,25 @@ console.log('Problem Solving Q:23 ');
 /*parseQueryString   */
 
 /*  
-Given a properly formatted URL, return the query string data as an array of key-value pairs, in the order that they appear.
+Given a properly formatted URL, return the query string data as an array of key-value pairs, 
+in the order that they appear.
 If the url does not contain a query string, return undefined
 */
 
-function parseQueryString() {
-  // YOUR CODE HERE
+function parseQueryString(string) {
+  const regExp = /[]/g;
+  const str = string.replace(regExp).split('%20').join(' ');
+  const arrStr = str.split('?');
+  if (arrStr.length > 1) {
+    const arr = arrStr.pop().split('&');
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] = arr[i].split('=');
+    }
+    return arr;
+  }
+  return undefined;
 }
-
-
-/* 
+/*
 Examples:
 parseQueryString("http://example.com?a=hello&b=99") // [ [ "a", "hello" ], [ "b", "99" ] ]
 parseQueryString("http://example.com?msg=hello%20world") // [ [ "msg", "hello world" ] ]
