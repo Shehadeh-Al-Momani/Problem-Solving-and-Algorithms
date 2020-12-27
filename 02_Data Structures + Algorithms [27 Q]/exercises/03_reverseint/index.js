8f15a57796c8763a20c6876ce59364af3905b321
Shehadeh-Al-Momani <shehadeh.almomani@gmail.com>
2020-12-27 16:52:17 +0200

    Solve Q3 Algorithms

--- 02_Data Structures + Algorithms [27 Q]/exercises/03_reverseint/index.js
+++ 02_Data Structures + Algorithms [27 Q]/exercises/03_reverseint/index.js
@@ -8,6 +8,8 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    return parseFloat(n.toString().split('').reverse().join(''))* Math.sign(n); //=> num * Math.sign(n) to get negative sign
+}
 
 module.exports = reverseInt;
